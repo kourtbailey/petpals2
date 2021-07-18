1420ae3d7febc0d8eb44af20fde4f3251d45d152
Kourt Bailey <kourt@kourt.cc>
2021-07-18 12:50:45 -0400

    More style changes to app.py

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -17,9 +17,7 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', '')
-                                         or "sqlite:///db.sqlite")
-# Remove tracking modifications
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
