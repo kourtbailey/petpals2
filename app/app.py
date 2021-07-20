9adf587d1346ec2f5f458f5f32d23490f50e47f6
Kourt Bailey <kourt@kourt.cc>
2021-07-20 18:37:47 -0400

    Reorganized app.py

--- app/app.py
+++ app/app.py
@@ -6,6 +6,8 @@ from flask import (
     jsonify,
     request,
     redirect)
+from flask_sqlalchemy import SQLAlchemy
+
 
 #################################################
 # Flask Setup
@@ -16,7 +18,6 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL')
     .replace('postgres://', 'postgresql://', 1)
@@ -29,28 +30,9 @@ db = SQLAlchemy(app)
 from .models import Pet
 
 
-# create route that renders index.html template
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
-
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
-
-    return render_template("form.html")
-
-
+#################################################
+# API Routes (start with '/api/')
+#################################################
 @app.route("/api/pals")
 def pals():
     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
@@ -76,6 +58,31 @@ def pals():
     }]
 
     return jsonify(pet_data)
+
+
+#################################################
+# Fontend Routes
+#################################################
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# Query the database and send the jsonified results
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        name = request.form["petName"]
+        lat = request.form["petLat"]
+        lon = request.form["petLon"]
+
+        pet = Pet(name=name, lat=lat, lon=lon)
+        db.session.add(pet)
+        db.session.commit()
+        return redirect("/", code=302)
+
+    return render_template("form.html")
 
 
 if __name__ == "__main__":
