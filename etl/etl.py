d21fbad40b01901ff535caad84e46ef0c59dc9ff
Kourt Bailey <kourt@kourt.cc>
2021-07-26 11:25:32 -0400

    More minor tweaks to CSV/SQLite

--- etl/etl.py
+++ etl/etl.py
@@ -3,11 +3,12 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Some of this might be best in a constants.py file and read here and in app.py
+# Use one or the other:
+SOURCE_PATH = 'etl/pets.csv'
+# SOURCE_PATH = 'sqlite:///etl/pets.sqlite'
+
 TABLE_NAME = 'pets'
 INDEX_COLUMN = 'id'
-SOURCE_PATH = 'sqlite:///etl/pets.sqlite'
-# SOURCE_PATH = 'etl/pets.csv'
 SOURCE_SQL = f"SELECT * FROM {TABLE_NAME};"
 
 # (https://help.heroku.com/ZKNTJQSK/
